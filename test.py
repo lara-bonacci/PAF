ace696a791ac9a7f403e4a3ceba38508fbf9fdcb
Lara Bonacci <lbonacci@pmfst.hr>
2023-03-03 12:39:17 +0100

    zadatak

---
+++ test.py
@@ -0,0 +1,18 @@
+#x=input("unesi koordinatu x")
+#y=input("unesi koordinatu y")
+
+#print("{}=k*{}+l".format(y,x))
+
+def pravac(X,Y):
+    return "{}=k*{}+l".format(Y,X)
+
+x=input("unesi koordinatu x")
+y=input("unesi koordinatu y")
+
+print(pravac(x,y))
+
+
+
+
+
+
