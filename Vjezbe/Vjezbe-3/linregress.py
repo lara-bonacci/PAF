341001878a88c2e707ed2466bd6e779c67c08135
lara-bonacci <lbonacci@pmfst.hr>
2023-03-27 09:25:44 +0200

    linregress d

--- Vjezbe/Vjezbe-3/linregress.py
+++ Vjezbe/Vjezbe-3/linregress.py
@@ -7,11 +7,10 @@ import statistics as s
 M= np.array([0.052, 0.124, 0.168, 0.236, 0.284, 0.336])
 phi= np.array([0.1745, 0.3491, 0.5236, 0.6981, 0.8727, 1.0472])
 
-D=M/phi
 
-a=s.mean(M*phi)/s.mean(phi**2)
-y=a*phi
-error=math.sqrt((s.mean(M**2)/s.mean(phi**2)-a**2)/len(M))
+D=s.mean(M*phi)/s.mean(phi**2)
+y=D*phi
+error=math.sqrt((s.mean(M**2)/s.mean(phi**2)-D**2)/len(M))
 
 print("Modul torzije aluminijske šipke: {}".format(D))
 print("Standardna pogreška procjene iznosi {}".format(error))
