f3b361ab374b129bc3c9bd825e2455021ee836f9
lara-bonacci <lbonacci@pmfst.hr>
2023-04-01 16:52:13 +0200

    predznaci u formuli za domet

--- Vjezbe/Vjezbe-4/gibanje.py
+++ Vjezbe/Vjezbe-4/gibanje.py
@@ -17,8 +17,8 @@ def domet(x_0,y_0,v,phi):
     v_y=v*np.sin(phi*math.pi/180)
     v_x=v*np.cos(phi*math.pi/180)
     a=-9.81
-    t=2*v_y/a
-    d=x_0 - v_x*t
+    t=-2*v_y/a
+    d=x_0 + v_x*t
     return d
 
 print(domet(0,0,10,60))
